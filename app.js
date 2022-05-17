2fe5ae1e4fa4db927b182724d7af5564041c4ba1
Ruben Poelen <ruben.poelen@hotmail.com>
2022-05-17 17:54:22 +0200

    authenticate fixed !

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
-
+const { isAuthenticated } = require("./middleware/jwt.middelware");
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
@@ -18,6 +18,9 @@ require("./config")(app);
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes/index.routes");
 app.use("/api", allRoutes);
+
+const authRouter = require("./routes/auth.routes"); //  <== IMPORT
+app.use("/auth", authRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- models/User.model.js
+++ models/User.model.js
@@ -3,11 +3,23 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    firstname: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      required: true,
     },
     password: String,
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+      lowercase: true,
+      trim: true,
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
