9aa4522a9c85f15bafba8e7b411489d2f15c3972
Ruben Poelen <ruben.poelen@hotmail.com>
2022-05-17 18:06:43 +0200

    fixed auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,13 @@
-const router = require("express").Router();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-// ℹ️ Handles password encryption
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
-
-// How many rounds should bcrypt run the salt (default [10 - 12 rounds])
+const router = express.Router();
 const saltRounds = 10;
 
 // Require the User model in order to interact with the database
 const User = require("../models/User.model");
-
+const { isAuthenticated } = require("../middleware/jwt.middelware");
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 
 const capitalize = require("../scripts/functions/capitalize");
@@ -109,10 +107,10 @@ router.post("/login", (req, res, next) => {
 
       if (passwordCorrect) {
         // Deconstruct the user object to omit the password
-        const { _id, email, name } = foundUser;
+        const { _id, email } = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, firstname, lastname };
+        const payload = { _id, email };
 
         // Create and sign the token
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
@@ -126,7 +124,24 @@ router.post("/login", (req, res, next) => {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
     })
-    .catch((err) => res.status(500).json({ message: "Internal Server Error" }));
+    .catch((err) =>
+      res.status(500).json({ message: "Internal Server Error", error: err })
+    );
 });
+
+// GET  /auth/verify  -  Used to verify JWT stored on the client
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  // <== CREATE NEW ROUTE
+
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
+
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json(req.payload);
+});
+
+module.exports = router;
 
 module.exports = router;
