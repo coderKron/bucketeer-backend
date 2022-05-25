bc08ff23dab5cd4b884d5c7bdecf5cb608ff0d2a
Louise <louisegabriellecc@gmail.com>
2022-05-25 19:17:58 +0100

    New Model: Story

---
+++ models/Story.model.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+const { default: mongoose } = require("mongoose");
+
+
+const storySchema = new Schema(
+  {
+    title: {
+      type: String,
+      requied: true,
+    },
+    kick: 
+      { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Kick",
+      },
+      content: String,
+      pictures: [{
+          type: String
+      }]
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Story = model("Story", storySchema);
+
+module.exports = Story
